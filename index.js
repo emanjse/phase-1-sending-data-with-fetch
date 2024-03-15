cb6653d06debda1ba1404c370c1f080fc9eafe98
Eman Jamal <emanjse@gmail.com>
2024-03-15 16:37:58 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        const newId = document.createElement("p");
+        newId.textContent = data.id;
+        document.body.appendChild(newId);
+      })
+      .catch(error => {
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+      });
+  }
